bf8b3665ce292430271f4f463fc12ae86786e6a8
Maria Vasileva <mariavasileva@Marias-MacBook-Air.local>
2022-10-21 18:15:53 +0300

    Added permissions.

--- api_yamdb/custom_auth/views.py
+++ api_yamdb/custom_auth/views.py
@@ -13,6 +13,7 @@ from users.models import User
 
 # class MyObtainTokenPairView(TokenObtainPairView):
 #     permission_classes = (AllowAny,)
+#
 #     serializer_class = MyTokenObtainPairSerializer
 
 
